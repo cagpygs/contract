54c7da9b4e6c36fa9b302735551d22aad1e754e6
cagpygs <cagpygs@gmail.com>
2026-03-03 04:30:26 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,9 @@
-
-import psycopg2
-
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="Irrigation",
-        user="postgres",
-        password="123456",
-        port="5432"
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT"),
+        sslmode="require"
     )
