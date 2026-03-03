bba7213c4ca35f87085064e0b0422aeeca6e1303
cagpygs <cagpygs@gmail.com>
2026-03-03 19:59:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -276,9 +276,9 @@ if not is_admin:
 
             is_master_form = (table == first_table)
 
+       
         with st.spinner("Loading section..."):
-        columns = get_table_columns(table, is_admin=False)
-        restore_draft_to_session(table, columns, user_id)
+    columns = get_table_columns(table, is_admin=False)
 
             form_data = {}
             filled_fields = 0
